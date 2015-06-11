f066aa605667028a9e206575745178cbe777e982
Allan Zhou <allan.zhou@berkeley.edu>
2015-06-11 01:37:27 -0700

    Added daily summary

--- popup.js
+++ popup.js
@@ -17,22 +17,32 @@ function makeReq(url, successCallback, unAuthCallback, errorCallback) {
   xhr.send(null);
 }
 
+function clear(node) {
+  while(node.hasChildNodes()) {
+    node.removeChild(node.lastChild);
+  }
+}
+
+function error(statusText) {
+  document.getElementById('status').textContent = statusText;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var dataBlock = document.getElementById('data');
-  dataBlock.textContent = 'Loading...';
-  makeReq('users/current', function(response) {
-    var userData = JSON.parse(response);
-    dataBlock.textContent = 'Hello ' + userData.data.full_name;
+  dataBlock.innerText = 'Loading...';
+  var now = new Date();
+  var dateFormatted = (now.getMonth() + 1)
+    + '/' + now.getDate() + '/' + now.getFullYear();
+  var params = '?start=' + dateFormatted + '&end=' + dateFormatted;
+  makeReq('users/current/summaries' + params, function(response) {
+    var summary = JSON.parse(response);
+    dataBlock.innerText = 'Today: ' + summary.data[0].grand_total.text;
   }, function() {
     var link = document.createElement('a');
     link.href = 'https://wakatime.com/login';
     link.innerText = 'Click here to login (new tab)';
     link.target = '_blank'; // open in new tab
-    dataBlock.textContent = 'Please make sure you are logged ' + 
-      'in to WakaTime from this browser';
     dataBlock.appendChild(document.createElement('br'))
     dataBlock.appendChild(link);
-  }, function(statusText) {
-    document.getElementById('status').textContent = statusText;
-  });
+  }, error);
 });
