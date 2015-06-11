ffcd48e6739ae055918aa63b6431789c503a3ae6
Allan Zhou <allan.zhou@berkeley.edu>
2015-06-11 14:34:59 -0700

    Display weekly data. Set popup size.

--- popup.js
+++ popup.js
@@ -24,20 +24,31 @@ function clear(node) {
 }
 
 function error(statusText) {
-  document.getElementById('status').textContent = statusText;
+  console.log('statusText');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   var dataBlock = document.getElementById('data');
-  dataBlock.innerText = 'Loading...';
+  var statusBar = document.getElementById('status');
   var now = new Date();
+  // current date in mm/dd/yyyy format
   var dateFormatted = (now.getMonth() + 1)
     + '/' + now.getDate() + '/' + now.getFullYear();
   var params = '?start=' + dateFormatted + '&end=' + dateFormatted;
   makeReq('users/current/summaries' + params, function(response) {
     var summary = JSON.parse(response);
     dataBlock.innerText = 'Today: ' + summary.data[0].grand_total.text;
+    // make call for weekly stats
+    makeReq('users/current/stats/last_7_days', function(response) {
+      clear(statusBar); // remove loading indicator
+      var weekStats = JSON.parse(response);
+      dataBlock.innerText += '\nThis week: ' + weekStats.data.human_readable_total;
+    }, function() {
+      console.log("Problem retrieving weekly data");
+    }, error);
   }, function() {
+    clear(statusBar);
+    // assume 401 means the user is not logged in
     var link = document.createElement('a');
     link.href = 'https://wakatime.com/login';
     link.innerText = 'Click here to login (new tab)';
