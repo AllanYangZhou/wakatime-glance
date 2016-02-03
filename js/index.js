f826be66b339ecb21c382229f5db522d4b2dd4f8
Allan Zhou <allan.zhou@berkeley.edu>
2016-02-03 00:27:20 -0800

    Correct the 7 day total

--- js/index.js
+++ js/index.js
@@ -4,6 +4,13 @@ wakatimeGlance.controller('PopupController', ['$http', function($http) {
   var popup = this;
   var WAKATIME_API_PREFIX = 'https://wakatime.com/api/v1/';
 
+  // Converts seconds to String with format: 'X hrs Y min.'
+  var secondsToHoursAndMinutes = function(seconds) {
+    var hours = _.floor(seconds / 3600);
+    var minutes = _.floor((seconds % 3600) / 60);
+    return _.join([String(hours), 'hrs', String(minutes), 'min'], ' ');
+  };
+
   // Format Date object into MM/DD/YYYY format for WakaTime API.
   var formatDate = function(date) {
     return _.join([
@@ -13,38 +20,46 @@ wakatimeGlance.controller('PopupController', ['$http', function($http) {
     ], '/');
   };
 
-  // Grab the statistics for a date and update the view.
-  var setStats = function(date) {
-    var dateFormatted = formatDate(date);
-    popup.humanReadableDate = dateFormatted;
-    popup.stats = '';
+  var getStatistics = function(startDate, endDate, callback) {
     var requestFormatted = WAKATIME_API_PREFIX +
-      'users/current/summaries?start=' + dateFormatted +
-      '&end=' + dateFormatted;
+      'users/current/summaries?start=' + startDate + '&end=' + endDate;
     $http.get(requestFormatted).then(function(response) {
-      popup.stats = response.data.data[0].grand_total.text;
+      var result = secondsToHoursAndMinutes(
+        _.sumBy(response.data.data, function(o) {
+          return o.grand_total.total_seconds
+        })
+      );
+      callback(result);
     }, function(err) {
       console.log(err);
     });
   };
 
-  // The date which we are displaying stats for.
+  // The single date which we are displaying stats for.
   var specifiedDate = new Date();
 
   popup.humanReadableDate = ''; // Human readable version of specifiedDate.
-  popup.stats = ''; // The actual statistics
-  popup.sevenDayStat = ''; // Weekly total
+  popup.specifiedStat = ''; // The statistics for specifiedDate.
+  popup.sevenDayStat = ''; // The statistics for the last seven days.
 
   // Decrease specifiedDate by one day, and update stats.
   popup.decrementDate = function() {
     specifiedDate.setDate(specifiedDate.getDate() - 1);
-    setStats(specifiedDate);
+    var formattedDate = formatDate(specifiedDate);
+    popup.humanReadableDate = formattedDate;
+    getStatistics(formattedDate, formattedDate, function(result) {
+      popup.specifiedStat = result;
+    });
   };
 
   // Increase specifiedDate by one day, and update stats.
   popup.incrementDate = function() {
     specifiedDate.setDate(specifiedDate.getDate() + 1);
-    setStats(specifiedDate);
+    var formattedDate = formatDate(specifiedDate);
+    popup.humanReadableDate = formattedDate;
+    getStatistics(formattedDate, formattedDate, function(result) {
+      popup.specifiedStat = result;
+    });
   };
 
   // Checks whether specifiedDate is the current date.
@@ -56,11 +71,17 @@ wakatimeGlance.controller('PopupController', ['$http', function($http) {
   };
 
   // Initialize stats.
-  setStats(specifiedDate);
-  var sevenDaysRequest = $http.get(WAKATIME_API_PREFIX +
-    'users/current/stats/last_7_days').then(function(response) {
-    popup.sevenDayStat = response.data.data.human_readable_total;
-  }, function(err) {
-    console.log(err);
+  var nowFormatted = formatDate(new Date());
+  popup.humanReadableDate = nowFormatted;
+  getStatistics(nowFormatted, nowFormatted, function(result){
+    popup.specifiedStat = result;
   });
+
+  var sixDaysAgo = new Date();
+  sixDaysAgo.setDate(sixDaysAgo.getDate() - 6);
+  getStatistics(
+    formatDate(sixDaysAgo), formatDate(new Date()), function(result) {
+      popup.sevenDayStat = result;
+    }
+  );
 }]);
